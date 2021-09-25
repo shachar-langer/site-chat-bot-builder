82d62cc3fdea61656e7a800c16114ba00b5aec8b
Shachar Langer <shachar.langer@gmail.com>
2021-09-25 17:09:42 +0400

    add missing configuration for the actions and for blocks of type 'text'

--- src/components/Block.vue
+++ src/components/Block.vue
@@ -24,6 +24,7 @@
         <div class="mb-2" v-if="blockType === 'text'">
             <label for="text">Text: </label>
             <input
+                v-model="text"
                 id="text"
                 type="text"
                 name="text"
@@ -61,6 +62,19 @@
         </div>
 
         <div v-if="actionType === 'yes-no'">
+
+            <div class="mb-2">
+                <label for="yes-label">Left button label: </label>
+                <input
+                    v-model="yesLabel"
+                    id="yes-label"
+                    type="text"
+                    name="yesLabel"
+                    placeholder="Please insert the label for the left button"
+                    class="w-full"
+                />
+            </div>
+
             <div class="mb-2">
                 <label for="next-block-yes">Next block for "yes": </label>
                 <select v-model="nextBlockYesId" name="next-block-yes" id="next-block-yes" class="w-full">
@@ -70,11 +84,49 @@
             </div>
 
             <div class="mb-2">
+                <label for="no-label">Right button label: </label>
+                <input
+                    v-model="noLabel"
+                    id="no-label"
+                    type="text"
+                    name="noLabel"
+                    placeholder="Please insert the label for the right button"
+                    class="w-full"
+                />
+            </div>
+
+            <div class="mb-2">
                 <label for="next-block-no">Next block for "no": </label>
                 <select v-model="nextBlockNoId" name="next-block-no" id="next-block-no" class="w-full">
                     <option value="">No next block</option>
                     <option v-for="(block, index) in allBlocks" :key="index" :value="block.id">{{block.name}}</option>
                 </select>
+            </div>
+        </div>
+
+        <div v-if="actionType === 'input'">
+            <div class="mb-2">
+                <label for="inputLabel">Label: </label>
+                <input
+                    v-model="inputLabel"
+                    id="inputLabel"
+                    type="text"
+                    name="inputLabel"
+                    placeholder="Please insert the input field's label"
+                    class="w-full"
+                />
+            </div>
+
+            <div class="mb-2">
+                <label for="inputPlaceholder">Placeholder: </label>
+                <input
+                    v-model="inputPlaceholder"
+                    id="inputPlaceholder"
+                    type="text"
+                    name="inputPlaceholder"
+                    placeholder="Please insert the input field's placeholder"
+                    class="w-full"
+                />
             </div>
         </div>
 
@@ -97,9 +149,14 @@ const props = defineProps({
 
 const name = ref('')
 const blockType = ref('text')
+const text = ref('')
+const inputLabel = ref('')
+const inputPlaceholder = ref('')
 const actionType = ref('')
 const nextBlockId = ref('')
+const yesLabel = ref('')
 const nextBlockYesId = ref('')
+const noLabel = ref('')
 const nextBlockNoId = ref('')
 
 const onSubmit = () => {
@@ -108,24 +165,55 @@ const onSubmit = () => {
     switch (actionType.value) {
         case 'input':
             action = {
-                type: actionType.value,
-                attributes: { nextBlockId: nextBlockId.value }
+                type: 'text-input',
+                attributes: {
+                    inputLabel,
+                    inputPlaceholder,
+                    nextBlockId: nextBlockId.value
+                }
             }
             break
         case 'yes-no':
             action = {
                 type: actionType.value,
                 attributes: {
-                    yes: { nextBlockId: nextBlockYesId.value },
-                    no: { nextBlockId: nextBlockNoId.value }
+                    yes: {
+                        label: yesLabel,
+                        nextBlockId: nextBlockYesId.value
+                    },
+                    no: {
+                        label: noLabel,
+                        nextBlockId: nextBlockNoId.value
+                    }
                 }
             }
             break
     }
+
+    let content
+    switch (blockType.value) {
+        case 'text':
+            content = {
+                type: blockType.value,
+                attributes: {
+                    text
+                }
+            }
+            break
+        case 'image':
+            content = {
+                type: blockType.value,
+                attributes: {
+                    fileName: ""
+                }
+            }
+            break
+    }
+
     emit('add', {
         id,
         name: name.value,
-        blockType: blockType.value,
+        content,
         action
     })
 }
